6c765ec2acf4bb7fd8c6c03ce1d5c35b8c5135a9
hashsh2000 <hashsh.net@gmail.com>
2025-12-01 16:25:46 +0900

    ok

--- components/AISystemHero.tsx
+++ components/AISystemHero.tsx
@@ -33,8 +33,8 @@ export default function AISystemHero() {
       opacity: number;
 
       constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.x = Math.random() * canvas!.width;
+        this.y = Math.random() * canvas!.height;
         this.vx = (Math.random() - 0.5) * 0.5;
         this.vy = (Math.random() - 0.5) * 0.5;
         this.radius = Math.random() * 2 + 1;
@@ -45,8 +45,8 @@ export default function AISystemHero() {
         this.x += this.vx;
         this.y += this.vy;
 
-        if (this.x < 0 || this.x > canvas.width) this.vx *= -1;
-        if (this.y < 0 || this.y > canvas.height) this.vy *= -1;
+        if (this.x < 0 || this.x > canvas!.width) this.vx *= -1;
+        if (this.y < 0 || this.y > canvas!.height) this.vy *= -1;
       }
 
       draw(ctx: CanvasRenderingContext2D) {
